7e5f2b3857134f4e9a6d937a37741051e06ae2a1
yeshwanth <yeshwanthds2002@gmail.com>
2024-10-22 23:43:10 +0530

    updated

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import Input from "../components/input";
+import Input from "../components/Input";
 import { Loader, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
